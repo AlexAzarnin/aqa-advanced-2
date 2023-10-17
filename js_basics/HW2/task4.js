a6cc8ca2ef962fdeea5ce0d1b516a2a184f3b7c6
Alex <azarnin.alex@gmail.com>
2023-10-17 15:55:12 +0300

    ДЗ 2. JS Basics

---
+++ js_basics/HW2/task4.js
@@ -0,0 +1,8 @@
+const age1 = 20;
+const age2 = 15;
+const adult1 = age1 >= 18;
+const adult2 = age2 >= 18;
+console.log(age1);
+console.log(adult1);
+console.log(age2);
+console.log(adult2);
---
+++ js_basics/HW2/task5.js
@@ -0,0 +1,14 @@
+const radius = 5;
+const areaCircle = (Math.PI * Math.pow(radius,2)).toFixed(2);
+console.log(`Area of circle is ${areaCircle}`);
+
+const length = 5;
+const width = 6;
+const areaSquare = (length * width).toFixed(2);
+console.log(`Area of square is ${areaSquare}`);
+
+
+const height = 10;
+const cylinderRadius = 5;
+const cylinderVolume = (Math.PI * Math.pow(cylinderRadius,2) * height).toFixed(2);
+console.log(`Cylinder volume is ${cylinderVolume}`);
